520fefd3e5c69b195c6dc71997bc20d26daf3626
Simon Gordon <gordonsimon@hotmail.com>
2018-09-24 19:28:32 +1000

    Add button to close current tab

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
+declare var chrome: any;
+
 console.log("Hello!");
+
+console.log(chrome);
+console.log(chrome.tabs);
+
+// // Content script
+// chrome.runtime.sendMessage({ closeThis: true });
+
+// // Background script
+// chrome.runtime.onMessage.addListener(function(
+//   message: { closeThis: boolean },
+//   sender: {
+//     tab: {
+//       id: number;
+//     };
+//   },
+//   sendResponse: {}
+// ) {
+//   if (message.closeThis) chrome.tabs.remove(sender.tab.id);
+// });
+
+const btnClick: HTMLElement = document.getElementById("btnClick")!;
+
+btnClick.addEventListener("click", function() {
+  console.log("Button was clicked");
+  console.log(chrome);
+  console.log(chrome.tabs);
+  chrome.tabs.getSelected(null, function(tab: { id: number }) {
+    chrome.tabs.remove(tab.id);
+  });
+});
