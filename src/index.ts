59df386ed1bdd88e709a71b3c0b48450486d9e38
Simon Gordon <gordonsimon@hotmail.com>
2018-10-12 18:19:10 +1000

    Add button to sort tabs

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,29 @@ btnClick.addEventListener("click", () => {
     });
   });
 });
+
+const btnSort: HTMLElement = document.getElementById("btnSort")!;
+
+btnSort.addEventListener("click", () => {
+  console.log("Button was clicked");
+
+  chrome.tabs.query({ currentWindow: true }, function(tabArray) {
+    const sorted = tabArray.sort(({ url: urlA = "" }, { url: urlB = "" }) => {
+      if (urlA == urlB) {
+        return 0;
+      }
+      if (urlA < urlB) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    console.log(sorted);
+    for (let i = 0; i < sorted.length; i++) {
+      const element = sorted[i];
+      chrome.tabs.move(element.id || 0, {
+        index: i
+      });
+    }
+  });
+});
